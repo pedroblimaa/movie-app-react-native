f25532aac97cf0c4ee5f7bccf314fcc43ef0b714
Pedro <pedroblimaa@gmail.com>
2025-10-13 18:24:34 -0300

    Add pages and navbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,86 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import {ImageBackground, StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+import {Tabs} from "expo-router";
+import {Image} from "expo-image";
+import {icons} from "@/constants/icons";
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+const TabIcon = ({focused, icon, title}: any) => {
+    if (!focused) {
+        return (
+            <View style={styles.tabView}>
+                <Image source={icon} tintColor='#A8B5DB'/>
+            </View>
+        )
+    }
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <ImageBackground>
+            <Image source={icon} tintColor='#151312'/>
+            <Text>{title}</Text>
+        </ImageBackground>
+    )
 }
+
+const _Layout = () => {
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarShowLabel: false,
+            }}
+        >
+            <Tabs.Screen
+                name='index'
+                options={{
+                    title: 'Home',
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon focused={focused} icon={icons.home} title='Home'/>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name='search'
+                options={{
+                    title: 'Search',
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon focused={focused} icon={icons.search} title='Search'/>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name='saved'
+                options={{
+                    title: 'Saved',
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon focused={focused} icon={icons.save} title='Saved'/>
+                    )
+                }}
+            />
+            <Tabs.Screen
+                name='profile'
+                options={{
+                    title: 'Profile',
+                    headerShown: false,
+                    tabBarIcon: ({focused}) => (
+                        <TabIcon focused={focused} icon={icons.person} title='Profile'/>
+                    )
+                }}
+            />
+        </Tabs>
+    )
+}
+export default _Layout
+const styles = StyleSheet.create({
+    tabView: {
+        flex: 1,
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minWidth: 10,
+        minHeight: 40,
+    }
+})
---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native'
+import React from 'react'
+
+const Movies = () => {
+    return (
+        <View>
+            <Text>Movie Details</Text>
+        </View>
+    )
+}
+export default Movies
+const styles = StyleSheet.create({})
